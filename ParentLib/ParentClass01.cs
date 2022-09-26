01e7a896b1e420624d96bc917b2ed4a499d6fff6
GreenMoose <juggernaut78@gmail.com>
2022-09-26 12:26:56 +0200

    Reference submodule Module01 [master]

---
+++ ParentLib/ParentClass01.cs
@@ -0,0 +1,15 @@
+﻿namespace ParentLib;
+
+using Module01.Lib;
+
+public class ParentClass01
+{
+    #region Public members
+
+    public static void Foo()
+    {
+        Module01Class.Foo();
+    }
+
+    #endregion
+}
